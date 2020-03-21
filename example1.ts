e31b47fb60e2883a96db9b8781880a28ab049c8b
Luis Geniole <luis.geniole@gmail.com>
2020-03-21 00:19:18 -0300

    Got the server to work.

--- example1.ts
+++ example1.ts
@@ -1,6 +1,22 @@
+/**
+ * https://deno.land
+ * 
+ * curl -fsSL https://deno.land/x/install/install.sh | sh
+ * brew install deno
+ * deno https://deno.land/std/examples/welcome.ts
+ * deno --allow-net index.ts
+ * 
+ * https://deno.land/std/manual.md#download-and-install
+ * https://github.com/denoland/deno_website2
+ * https://github.com/denoland/rusty_v8
+ * 
+ * https://deno.land/typedoc/
+ * 
+ */
+
 console.log("Welcome to Deno 🦕");
 
-import { serve } from "https://deno.land/std@v0.30.0/http/server.ts";
+import { serve } from "https://deno.land/std@v0.36.0/http/server.ts";
 const s = serve({ port: 8000 });
 console.log("http://localhost:8000/");
 for await (const req of s) {
